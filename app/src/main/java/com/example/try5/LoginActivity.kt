cd240385208e8a08ef19c38025dee2311d24a646
PABEHTO <nyakushoku@gmail.com>
2025-03-21 03:11:12 +1000

    second:buttons_etc

--- app/src/main/java/com/example/try5/LoginActivity.kt
+++ app/src/main/java/com/example/try5/LoginActivity.kt
@@ -4,6 +4,11 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.ImageButton
+import android.content.Intent
+import android.text.method.PasswordTransformationMethod
+import android.text.method.SingleLineTransformationMethod
+import android.widget.EditText
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,8 +16,29 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         val continueButton: Button = findViewById(R.id.continue_button)
+        val backButton: ImageButton = findViewById(R.id.back_button)
+
+        val passwordEditText: EditText = findViewById(R.id.password_edit_text)
         continueButton.setOnClickListener {
             Toast.makeText(this, "Вход (пока что заглушка)", Toast.LENGTH_SHORT).show()
+        }
+        backButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        var isPasswordVisible = false
+        passwordEditText.setOnClickListener {
+            if (isPasswordVisible) {
+                passwordEditText.transformationMethod = PasswordTransformationMethod.getInstance()
+                passwordEditText.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_visibility, 0)
+            } else {
+                passwordEditText.transformationMethod = SingleLineTransformationMethod.getInstance()
+                passwordEditText.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_visibility_off, 0)
+            }
+            isPasswordVisible = !isPasswordVisible
+            passwordEditText.setSelection(passwordEditText.text.length)
         }
     }
 }
--- app/src/main/java/com/example/try5/RegisterActivity.kt
+++ app/src/main/java/com/example/try5/RegisterActivity.kt
@@ -1,9 +1,16 @@
 package com.example.try5
 
+import android.content.Context
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.ImageButton
+import android.content.Intent
+import android.text.method.PasswordTransformationMethod
+import android.text.method.SingleLineTransformationMethod
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,8 +18,43 @@ class RegisterActivity : AppCompatActivity() {
         setContentView(R.layout.activity_register)
 
         val continueButton: Button = findViewById(R.id.continue_button)
+        val backButton: ImageButton = findViewById(R.id.back_button)
+
+        val passwordEditText: EditText = findViewById(R.id.password_edit_text)
+        val confirmPasswordEditText: EditText = findViewById(R.id.confirm_password_edit_text)
         continueButton.setOnClickListener {
             Toast.makeText(this, "Регистрация (пока что заглушка)", Toast.LENGTH_SHORT).show()
+        }
+        backButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        var isPasswordVisible = false
+        passwordEditText.setOnClickListener {
+            if (isPasswordVisible) {
+                passwordEditText.transformationMethod = PasswordTransformationMethod.getInstance()
+                passwordEditText.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_visibility, 0)
+            } else {
+                passwordEditText.transformationMethod = SingleLineTransformationMethod.getInstance()
+                passwordEditText.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_visibility_off, 0)
+            }
+            isPasswordVisible = !isPasswordVisible
+            passwordEditText.setSelection(passwordEditText.text.length) // Курсор в конец
+        }
+
+        var isConfirmPasswordVisible = false
+        confirmPasswordEditText.setOnClickListener {
+            if (isConfirmPasswordVisible) {
+                confirmPasswordEditText.transformationMethod = PasswordTransformationMethod.getInstance()
+                confirmPasswordEditText.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_visibility, 0)
+            } else {
+                confirmPasswordEditText.transformationMethod = SingleLineTransformationMethod.getInstance()
+                confirmPasswordEditText.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_visibility_off, 0)
+            }
+            isConfirmPasswordVisible = !isConfirmPasswordVisible
+            confirmPasswordEditText.setSelection(confirmPasswordEditText.text.length)
         }
     }
 }
